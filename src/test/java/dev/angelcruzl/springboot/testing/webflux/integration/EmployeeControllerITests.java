4bdd59d5474584175e91074a32f9d5b2531926a6
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-05-05 20:30:22 -0600

    Add update employee integration test

--- src/test/java/dev/angelcruzl/springboot/testing/webflux/integration/EmployeeControllerITests.java
+++ src/test/java/dev/angelcruzl/springboot/testing/webflux/integration/EmployeeControllerITests.java
@@ -3,6 +3,7 @@ package dev.angelcruzl.springboot.testing.webflux.integration;
 import dev.angelcruzl.springboot.testing.webflux.dto.EmployeeDto;
 import dev.angelcruzl.springboot.testing.webflux.repository.EmployeeRepository;
 import dev.angelcruzl.springboot.testing.webflux.service.EmployeeService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
+
+import java.util.Collections;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class EmployeeControllerITests {
@@ -31,7 +34,10 @@ public class EmployeeControllerITests {
         employeeDto.setFirstName("Angel");
         employeeDto.setLastName("Cruz");
         employeeDto.setEmail("me@angelcruzl.dev");
+    }
 
+    @AfterEach
+    public void tearDown() {
         repository.deleteAll().subscribe();
     }
 
@@ -81,6 +87,29 @@ public class EmployeeControllerITests {
                 .expectStatus().isOk()
                 .expectBodyList(EmployeeDto.class)
                 .hasSize(2);
+    }
+
+    @Test
+    public void testUpdateEmployee() {
+        EmployeeDto savedEmployee = service.saveEmployee(employeeDto).block();
+
+        EmployeeDto updatedEmployee = new EmployeeDto();
+        updatedEmployee.setFirstName("Luis");
+        updatedEmployee.setLastName("Lara");
+        updatedEmployee.setEmail("test@mail.com");
+
+        webTestClient.put().uri("/api/v1/employees/{id}", Collections.singletonMap("id", savedEmployee.getId()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(updatedEmployee), EmployeeDto.class)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(System.out::println)
+                .jsonPath("$.id").isEqualTo(savedEmployee.getId())
+                .jsonPath("$.firstName").isEqualTo("Luis")
+                .jsonPath("$.lastName").isEqualTo("Lara")
+                .jsonPath("$.email").isEqualTo(updatedEmployee.getEmail());
     }
 
 }
